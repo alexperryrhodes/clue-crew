4b282b2842999fd3cdde40e38b30d39458c8ef0f
Alexandra Rhodes <atperry30@gmail.com>
2023-04-10 12:12:51 -0700

    Added routing logic to app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useRoutes, Link } from 'react-router-dom'
 import './App.css'
+import CreateCrew from '../pages/CreateCrew'
+import ReadCrew from '../pages/ReadCrew'
+import { supabase } from './client'
 
 function App() {
 
+  const [crew, setCrew] = useState([]);
+
+  useEffect(() => {
+    
+    const fetchCrew = async () => {
+      const {data} = await supabase
+      .from('ClueCrew')
+      .select()
+      .order('created_at', { ascending: true })
+
+      // set state of posts
+      setCrew(data)
+
+    }
+
+    fetchCrew()
+
+  }, []);
+
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/",
+      element:<ReadCrew data={crew}/>
+    },
+    // {
+    //   path:"/edit/:id",
+    //   element: <EditPost data={posts} />
+    // },
+    {
+      path:"/create",
+      element: <CreateCrew />
+    }
+  ]);
+
   return (
     <div className="App">
+      <h1>Clue Crew</h1>
+      <Link to="/"><button className="navButton"> View Crew Crew </button></Link>
+      <Link to="/create"><button className="navButton"> Create New Crew Member</button></Link>
+      {element}
     </div>
   )
 }
