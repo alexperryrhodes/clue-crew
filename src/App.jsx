6a6895edfd49586bad0f571afad3843aa3549050
Alexandra Rhodes <atperry30@gmail.com>
2023-04-10 12:52:29 -0700

    Updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,60 @@
-import { useState, useEffect } from 'react'
-import { useRoutes, Link } from 'react-router-dom'
-import './App.css'
-import CreateCrew from '../pages/CreateCrew'
-import ReadCrew from '../pages/ReadCrew'
-import { supabase } from './client'
+import { useState, useEffect } from "react";
+import { useRoutes, Link } from "react-router-dom";
+import "./App.css";
+import CreateCrew from "../pages/CreateCrew";
+import ReadCrew from "../pages/ReadCrew";
+import EditCrew from "../pages/EditCrew";
+import DetailCrew from "../pages/DetailCrew";
+import { supabase } from "./client";
 
 function App() {
-
   const [crew, setCrew] = useState([]);
 
   useEffect(() => {
-    
     const fetchCrew = async () => {
-      const {data} = await supabase
-      .from('ClueCrew')
-      .select()
-      .order('created_at', { ascending: true })
+      const { data } = await supabase
+        .from("ClueCrew")
+        .select()
+        .order("created_at", { ascending: true });
 
-      // set state of posts
-      setCrew(data)
+      setCrew(data);
+    };
 
-    }
-
-    fetchCrew()
-
+    fetchCrew();
   }, []);
 
-
-  // Sets up routes
   let element = useRoutes([
     {
       path: "/",
-      element:<ReadCrew data={crew}/>
+      element: <ReadCrew data={crew} />,
     },
-    // {
-    //   path:"/edit/:id",
-    //   element: <EditPost data={posts} />
-    // },
     {
-      path:"/create",
-      element: <CreateCrew />
-    }
+      path: "/edit/:id",
+      element: <EditCrew data={crew} />,
+    },
+    {
+      path: "/:id",
+      element: <DetailCrew data={crew} />,
+    },
+    {
+      path: "/create",
+      element: <CreateCrew />,
+    },
+
   ]);
 
   return (
     <div className="App">
       <h1>Clue Crew</h1>
-      <Link to="/"><button className="navButton"> View Crew Crew </button></Link>
-      <Link to="/create"><button className="navButton"> Create New Crew Member</button></Link>
+      <Link to="/">
+        <button className="navButton"> View Crew Crew </button>
+      </Link>
+      <Link to="/create">
+        <button className="navButton"> Create New Crew Member</button>
+      </Link>
       {element}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
