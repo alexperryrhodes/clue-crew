779afd0873cb38e7448bfae348f3d25623921d08
Alexandra Rhodes <atperry30@gmail.com>
2023-04-10 12:51:38 -0700

    Added buttons to cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 const Card = (props) => {
   return (
     <div className="Card">
-      {/* <Link to={'edit/'+ props.id}><img className="moreButton" alt="edit button" src={more} /></Link> */}
       <img className = "magnifyImg" src="../magnifyingglass@40x.png"/>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
       <p>Skill Level: {props.skillLevel}</p>
       <p>Debut: {props.debutNovel}</p>
+      <Link to={'edit/' + props.id}><button className="editButton">Edit</button></Link>
+      <Link to={'/'+ props.id}><button className="editButton">More Info</button></Link>
     </div>
   );
 };
