7f6a629c6615e1a9240d9267d52ac1b8f8a41c94
Alexandra Rhodes <atperry30@gmail.com>
2023-04-10 12:13:22 -0700

    Renamed client file

--- src/client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://miurodymevulaljhvhzs.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pdXJvZHltZXZ1bGFsamh2aHpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEwNzE4MjcsImV4cCI6MTk5NjY0NzgyN30.jKCgXhCwYq6hzw4M60a4stztbfzsL0ejwh-Lazskx-Y';
-
-export const supabase = createClient(URL, API_KEY);
