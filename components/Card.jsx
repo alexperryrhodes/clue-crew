ba3c66a0661b191d11a48f19db4fef48b52355e6
Alexandra Rhodes <atperry30@gmail.com>
2023-04-10 13:04:29 -0700

    Removed detail from card

--- components/Card.jsx
+++ components/Card.jsx
@@ -7,8 +7,8 @@ const Card = (props) => {
       <img className = "magnifyImg" src="../magnifyingglass@40x.png"/>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>Skill Level: {props.skillLevel}</p>
-      <p>Debut: {props.debutNovel}</p>
+      {/* <p>Skill Level: {props.skillLevel}</p>
+      <p>Debut: {props.debutNovel}</p> */}
       <Link to={'edit/' + props.id}><button className="editButton">Edit</button></Link>
       <Link to={'/'+ props.id}><button className="editButton">More Info</button></Link>
     </div>
